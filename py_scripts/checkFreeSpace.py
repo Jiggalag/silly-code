c481d008feeac2686c7aa9abe92ba5c14fdf0e56
Pavel Kiselev <pavel.kiselev@best4ad.com>
2016-11-24 12:02:42 +0400

    Added script for alerting in case of free space on server ended.

---
+++ py_scripts/checkFreeSpace.py
@@ -0,0 +1,14 @@
+__author__ = 'pavel.kiselev'
+
+import subprocess
+
+user = 'pavel.kiselev'
+server = 'dev02.maxifier.com'
+threshold = 90
+pathToImage = '/home/jiggalag/fffuuu.jpg'
+getFreeSpace = "ssh %s@%s 'df -h | grep /dev/sdb1'" % (user, server)
+freeSpace = subprocess.check_output(getFreeSpace, shell=True, universal_newlines=True)
+percent = freeSpace[freeSpace.find('%') - 2:freeSpace.find('%')]
+if int(percent) >= threshold:
+    openImage = 'xdg-open %s' % pathToImage
+    subprocess.call(openImage, shell=True)
