c90ab69850e94c1362a413182957a892b70f6bd7
Pavel Kiselev <pavel.kiselev@best4ad.com>
2017-06-09 16:23:19 +0400

    Writed tor-viewer purposed for generate impressions with different user-agent and geo-location.

--- py_scripts/tor-example.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.PhantomJS() # or add to your PATH
-driver.set_window_size(1024, 768) # optional
-driver.get('https://google.com/')
-driver.save_screenshot('screen.png') # save a screenshot to disk
--- py_scripts/torViewer.py
+++ py_scripts/torViewer.py
@@ -1,8 +1,16 @@
 import os
+from helpers import loggingHelper
 from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from stem import Signal
+from stem import CircStatus
+from stem.control import Controller
 
+logLevel = 20
+
+logger = loggingHelper.Logger(logLevel)
+
+amount = 5
 ua = [
     "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36",
     "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0",
@@ -18,21 +26,40 @@ ua = [
     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36 OPR/40.0.2308.62"
 ]
 
-# dcap = dict(DesiredCapabilities.PHANTOMJS)
+item = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36 OPR/40.0.2308.62"
 
-# path to the firefox binary inside the Tor package
+port = "8118"
+
+service_args = [
+    '--proxy=127.0.0.1:' + port,
+    '--proxy-type=http',
+    ]
+
+def changeIp():
+    with Controller.from_port(port=9051) as controller:
+        controller.authenticate(password="pokute713")
+        controller.signal(Signal.NEWNYM)
+
+        for circ in controller.get_circuits():
+            if circ.status != CircStatus.BUILT:
+                continue
+        exitFp, exitNickname = circ.path[-1]
+        exitDesc = controller.get_network_status(exitFp, None)
+        exitAddress = exitDesc.address if exitDesc else 'unknown'
+        logger.info("Ip successfully changed. New address is {}".format(exitAddress))
+
 binary = "/home/jiggalag/Downloads/tor-browser_ru/Browser/TorBrowser/Tor"
 if os.path.exists(binary) is False:
     raise ValueError("The binary path to Tor firefox does not exist.")
-firefox_binary = FirefoxBinary(binary)
 dcap = dict(DesiredCapabilities.FIREFOX)
 
-
-for i in range(3):
+for i in range(amount):
     for item in ua:
         dcap["phantomjs.page.settings.userAgent"] = (item)
-        driver = webdriver.PhantomJS(desired_capabilities=dcap)
-        driver.set_window_size(1024, 768) # optional
+        driver = webdriver.PhantomJS(desired_capabilities=dcap, service_args=service_args)
+        driver.set_window_size(1024, 768)
+        changeIp()
         driver.get('http://inventale.com/en')
-        driver.quit()
-    #    driver.save_screenshot('screen.png') # save a screenshot to disk
+        driver.delete_all_cookies()
+        driver.execute_script('localStorage.clear();')
+        driver.close()
