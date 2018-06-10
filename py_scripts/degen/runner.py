b87a6e121f13f0f16ed01b860c6de2e8f109edb1
Pavel Kiselev <pavel.kiselev@inventale.com>
2018-06-10 19:14:00 +0400

    Added TableCloner class.

--- py_scripts/degen/runner.py
+++ py_scripts/degen/runner.py
@@ -1,12 +1,18 @@
+import argparse
 from target_parser import TargetParser
 from table_cloner import TableCloner
 from py_scripts.helpers.dbHelper import DbConnector
 from py_scripts.helpers.logging_helper import Logger
 
+parser = argparse.ArgumentParser(description='Make forecast great again')
+parser.add_argument('liquibase_password', type=str,
+                    help='cli location (default: "cli")')
+
+args = parser.parse_args()
 
 server = 'samaradb03.maxifier.com'
-user = 'itest'
-password = 'ohk9aeVahpiz1wi'
+user = 'liquibase'
+password = args.liquibase_password
 source_db = 'rick'
 target_db = 'rick_quality'
 logger = Logger('DEBUG')
@@ -18,10 +24,30 @@ connection_params = {
     'db': None
 }
 
+tables = [
+    'rickcampaign',
+    'rickcreative',
+    # 'rickcreativesiteplacereport',
+    'rickformat',
+    # 'rickformat_rickplace',
+    'rickformattype',
+    'rickgender',
+    'rickgeo',
+    # 'rickgeo_rickgeo',
+    'rickincome',
+    'rickinsertionorder',
+    'rickinterest',
+    'rickplace',
+    'rickplacereport',
+    'rickposition',
+    'ricksection',
+    'ricktarget'
+]
 
 connection = DbConnector(connection_params, logger).get_connection()
 parser = TargetParser(connection, source_db, logger)
 targeting = parser.get_targeting()
-tables = list()
-tt = TableCloner(connection, source_db, target_db, tables, logger)
+cloner = TableCloner(connection, source_db, target_db, tables, logger)
+cloner.create_tables()
+cloner.drop_tables()
 print('debug...')
--- py_scripts/degen/table_cloner.py
+++ py_scripts/degen/table_cloner.py
@@ -2,24 +2,72 @@ import datetime
 
 
 class TableCloner:
-    def __init__(self, connection, source_db, target_db, table_list, logger):
+    def __init__(self, connection, source_db, target_db, table_to_copy, logger):
         self.connection = connection
         self.source_db = source_db
         self.target_db = target_db
-        self.table_list = table_list
+        self.table_to_copy = table_to_copy
+        self.tables = self.get_table_list()
+        self.today = datetime.datetime.date(datetime.datetime.today())
         self.logger = logger
 
     def create_tables(self):
         create_queries = list()
         insert_queries = list()
-        for table in self.table_list:
-            create_query = "CREATE TABLE {0}{1} LIKE {2}.{0}".format(table, datetime.datetime.now(), self.source_db)
-            create_queries.append(create_query)
-            insert_query = "INSERT INTO {0} SELECT * FROM {1}.{0}".format(table, self.source_db)
-            insert_queries.append(insert_query)
+        go = self.check_date(self.today)
+        if go:
+            for table in self.table_to_copy:
+                table_name = "{}.{}{}".format(self.target_db, table, self.today).replace('-', '_')
+                create_query = "CREATE TABLE {} LIKE {}.{};".format(table_name, self.source_db, table)
+                create_queries.append(create_query)
+                insert_query = "INSERT INTO {} SELECT * FROM {}.{};".format(table_name, self.source_db, table)
+                insert_queries.append(insert_query)
+            with self.connection.cursor() as cursor:
+                for query in create_queries:
+                    self.logger.debug(query)
+                    cursor.execute(query)
+                for query in insert_queries:
+                    self.logger.debug(query)
+                    cursor.execute(query)
+                    self.connection.commit()
+        else:
+            self.logger.warn('Tables for date {} already copied in db {}'.format(self.today, self.target_db))
+
+    def check_date(self, today):
+        string_date = self.tables[-1][-10:].replace('_', '-')
+        last_date = datetime.datetime.date(datetime.datetime.strptime(string_date, "%Y-%m-%d"))
+        if last_date == today:
+            return False
+        else:
+            return True
+
+    def get_table_list(self):
+        query = "SHOW TABLES IN {};".format(self.target_db)
         with self.connection.cursor() as cursor:
-            for query in create_queries:
+            cursor.execute(query)
+            result = cursor.fetchall()
+            table_list = list()
+            for table in result:
+                table_list.append(table.get('Tables_in_{}'.format(self.target_db)))
+            table_list.sort()
+            return table_list
+
+    def drop_tables(self):
+        drop_list = self.find_old_tables()
+        query_list = list()
+        for table in drop_list:
+            query_list.append("DROP TABLE {}.{};".format(self.target_db, table))
+        for query in query_list:
+            with self.connection.cursor() as cursor:
                 cursor.execute(query)
-            for query in insert_queries:
-                cursor.execute(query)
-                self.connection.commit()
+                self.logger('Table {}.{} successfully dropped...'.format(self.target_db, table))
+
+
+    def find_old_tables(self):
+        delete_date = self.today - datetime.timedelta(days=30)
+        for table in self.tables:
+            if str(delete_date) in table:
+                start_index = self.tables.index(table)
+                # TODO: check this code!
+                return self.tables[:start_index - 1]
+        self.logger.info('There is no tables for deleting...')
